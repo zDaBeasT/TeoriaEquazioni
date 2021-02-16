74b5e002bd94523fcd8d262553a5ae295fd353fa
Daniele Hanelli <hanellidaniele@gmail.com>
2021-02-16 13:13:17 +0100

    Aggiornamento Lavoro 16.02

--- EquazioniLibrary.Test/EquazioniTest.cs
+++ EquazioniLibrary.Test/EquazioniTest.cs
@@ -9,8 +9,62 @@ namespace EquazioniLibrary.Test
         [TestMethod]
         public void IsDeterminedTest()
         {
+            double a = 4;
+            bool ValoreAspettato = true;
+            bool ValoreRitorno = Equazioni.IsDetermined(a);
+            Assert.AreEqual(ValoreAspettato, ValoreRitorno);
 
+        }
+        [TestMethod]
+        public void IsDeterminedTest1()
+        {
+            double a = 0;
+            bool ValoreAspettato = false;
+            bool ValoreRitorno = Equazioni.IsDetermined(a);
+            Assert.AreEqual(ValoreAspettato, ValoreRitorno);
 
+        }
+        [TestMethod]
+        public void IsInconsistedTest()
+        {
+            double a = 5, b = 3;
+            bool ValoreAspettato = false;
+            bool ValoreRitorno = Equazioni.IsInconsisted(a, b);
+            Assert.AreEqual(ValoreAspettato, ValoreRitorno);
+
+        }
+        [TestMethod]
+        public void IsInconsistedTest1()
+        {
+            double a = 0, b = 5;
+            bool ValoreAspettato = true;
+            bool ValoreRitorno = Equazioni.IsInconsisted(a, b);
+            Assert.AreEqual(ValoreAspettato, ValoreRitorno);
+
+        }
+        [TestMethod]
+        public void IsDegree2()
+        {
+            double a = 3, b = 2;
+            bool ValoreAspettato = true;
+            bool Risultato = Equazioni.IsDegree(b);
+            Assert.AreEqual(ValoreAspettato, Risultato);
+        }
+        [TestMethod]
+        public void Delta()
+        {
+            double a = 3, b = 2, c = 4;
+            double ValoreAspettato = -44;
+            double Risultato = Equazioni.Delta(a, b, c);
+            Assert.AreEqual(ValoreAspettato, Risultato);
+        }
+        [TestMethod]
+        public void TestEquazioneSecondoCompleta()
+        {
+            double a = 1, b = 2, c = 3;
+            string valoreAspettato = "Non ci sono punti di intersezione";
+            string valoreRitorno = Equazioni.Eq2Grado(a, b, c);
+            Assert.AreEqual(valoreAspettato, valoreRitorno);
         }
     }
 }
--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -4,9 +4,48 @@ namespace EquazioniLibrary
 {
     public class Equazioni
     {
-        public static bool IsDetermined(double )
+        public static bool IsDetermined(double a)
         {
-
+            if(a != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsInconsisted(double a, double b)
+        {
+            if (a==0 && b!=0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsDegree(double c)
+        {
+            return c == 2;
+        }
+        public static double Delta(double a, double b, double c)
+        {
+            return (b * b) - (4 * a * c);
+        }
+        public static string Eq2Grado(double a, double b, double c)
+        {
+            double x1, x2;
+            double delta = Delta(a, b, c), radiceDelta;
+            if(delta > 0)
+            {
+                radiceDelta = Math.Sqrt(delta);
+                x1 = (-b + radiceDelta) / (2 * a);
+                x2 = (-b - radiceDelta) / (2 * a);
+                return $"x1-{x1} x2-{x2}";
+            }else if (delta == 0)
+            {
+                return $"{-b / 2 * a}";
+            }
+            else
+            {
+                return "Non ci sono punti di intersezione";
+            }
         }
     }
 }
